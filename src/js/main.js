b9f7f4c031f7e3d4708fa6131ce9ea1c46180907
markdturner <mark.turner@newcastle.ac.uk>
2023-02-11 11:56:44 +0000

    first working version

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,49 @@
+function updateLabel(event) {
+  event.preventDefault()
+  const fakePath = document.getElementById('file-input').value.split('\\')
+  document.getElementById('file-label').textContent = fakePath[fakePath.length - 1]
+}
+
+function callPredict(event) {
+  
+  event.preventDefault()
+
+  var requestOptions = {
+    method: 'POST',
+    body: new FormData(event.target),
+    redirect: 'follow'
+  }
+
+  document.getElementById('form').classList.toggle('hide')
+  document.getElementById('predicting').classList.toggle('hide')
+
+  fetch("https://mousemapp.azurewebsites.net/api/predict?code=72zqkChgmomeOZmmwH_-wT1fg6nxykamnJTExzALnKccAzFufDWatg==", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+
+      const resultScore = document.getElementById('result-score')
+
+      resultScore.textContent = result.bcs
+      resultScore.classList.remove(...resultScore.classList)
+      resultScore.classList.add(`grade-${result.bcs}`)
+
+      document.getElementById('result-name').textContent = result.name
+      document.getElementById('result-room').textContent = result.room
+      document.getElementById('result-cage').textContent = result.cage
+      document.getElementById('result-mouseID').textContent = result.mouseID
+      document.getElementById('result-file').textContent = result.files[0].filename
+
+      document.getElementById('predicting').classList.toggle('hide')
+      document.getElementById('result').classList.toggle('hide')
+    })
+    .catch(error => Sentry.captureException(error))
+}
+
+function reset() {
+  document.getElementById('form').classList.toggle('hide')
+  document.getElementById('result').classList.toggle('hide')
+}
+
 (function () {
   const doc = document.documentElement
 
@@ -9,7 +55,7 @@
     /* global ScrollReveal */
     const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.hero-title, .hero-paragraph, .newsletter-header, .newsletter-form', {
+    sr.reveal('.hero-title, .hero-paragraph, .submission-header, .submission-form', {
       duration: 1000,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
@@ -33,4 +79,9 @@
       viewFactor: 0.5
     })
   }
+
+  const form = document.getElementById('form')
+  const files = document.getElementById('file-input')
+  form.addEventListener('submit', callPredict)
+  files.addEventListener('change', updateLabel)
 }())
